6074f5d071d837b0bb7f9fb0a4e788fdf220421d
William Felker <gndclouds@icloud.com>
2023-05-29 21:27:40 -0700

    styling

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -6,25 +6,24 @@ export const generateStaticParams = async () =>
   allBlogs.map((blog) => ({ slug: blog._raw.flattenedPath }));
 
 export const generateMetadata = ({ params }) => {
-  console.log("HI");
-  const blog = allBlogs.find((blog) => blog._raw.flattenedPath === params.slug);
-  return { title: blog.title };
+   const blog = allBlogs.find((blog) => blog._raw.flattenedPath === params.slug);
+  // return { title: blog.title };
 };
 
 const BlogLayout = ({ params }: { params: { slug: string } }) => {
   const blog = allBlogs.find((blog) => blog._raw.flattenedPath === params.slug);
-
-  const Content = getMDXComponent(blog.body.code);
+console.log(params)
+  // const Content = getMDXComponent(blog.body.code);
 
   return (
     <article className="py-8 mx-auto max-w-xl">
       <div className="mb-8 text-center">
-        <time dateTime={blog.date} className="mb-1 text-xs text-gray-600">
+        {/* <time dateTime={blog.date} className="mb-1 text-xs text-gray-600">
           {format(parseISO(blog.date), "LLLL d, yyyy")}
-        </time>
+        </time> */}
         <h1>{blog.title}</h1>
       </div>
-      <Content />
+      {/* <Content /> */}
     </article>
   );
 };
--- app/thoughts/page.tsx
+++ app/thoughts/page.tsx
@@ -4,40 +4,46 @@ import { Inter } from "next/font/google";
 
 import { compareDesc, format, parseISO } from "date-fns";
 import { allLogs, Log } from "contentlayer/generated";
+import { allBlogs, Blog } from "contentlayer/generated";
+import { allNewsletters, Newsletter } from "contentlayer/generated";
+
 import { getMDXComponent } from "next-contentlayer/hooks";
 
-function Card(log: Log) {
-  const Content = getMDXComponent(log.body.code);
+function Card(data: Data) {
 
   return (
     <div className="mb-8">
-      {/* <h2 className="text-xl">
+       <div className="text-xl">
         <Link
-          href={log.url}
-          className="text-blue-700 hover:text-blue-900"
-          legacyBehavior
+          href={data.url}
         >
-          {log.title}
+         <div className="text-lg">{data.title}</div>
         </Link>
-      </h2> */}
-      <time dateTime={log.publishedAt} className="block mb-2  text-gray-600">
-        {format(parseISO(log.publishedAt), "yyyy-MM-d, ")}
+      </div>
+      <time dateTime={data.publishedAt} className="text-sm">
+        {format(parseISO(data.publishedAt), "yyyy-MM-d, ")}
       </time>
-      <div className=""> </div>
     </div>
   );
 }
 
 export default function Home() {
+  // Sort by Date
   const logs = allLogs.sort((a, b) =>
+    compareDesc(new Date(a.publishedAt), new Date(b.publishedAt))
+  );
+  const blogs = allBlogs.sort((a, b) =>
+    compareDesc(new Date(a.publishedAt), new Date(b.publishedAt))
+  );
+  const newsletters = allNewsletters.sort((a, b) =>
     compareDesc(new Date(a.publishedAt), new Date(b.publishedAt))
   );
   return (
     <main className="">
       <div className="mb-9">
-        <div className="pb-3 font-bold">Thoughts</div>
-        <div className="">
-          Dolor sint cupidatat sunt mollit officia reprehenderit sit minim sint
+        <div className="pb-3 text-lg font-bold">Thoughts</div>
+        <div className="text-base">
+          Sint cupidatat sunt mollit officia reprehenderit sit minim sint
           consectetur sint cillum minim nostrud duis. Culpa sunt esse eu
           occaecat nulla incididunt amet reprehenderit tempor esse sunt nisi.
           Sint mollit laborum exercitation ullamco laborum sint proident ea
@@ -46,27 +52,32 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="border border-2 mb-9">
-        <div className="font-bold">blog</div>
-        <div className="grid gap-4 grid-cols-3 grid-rows-3">
-          {logs.map((log, idx) => (
-            <Card key={idx} {...log} />
+      {/* Section Blogs */}
+      <div className="mb-9">
+        <div className="font-bold text-lg">blog</div>
+        <div className="grid gap-2 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 grid-rows-3">
+          {blogs.map((data, idx) => (
+            <Card key={idx} {...data} />
           ))}
         </div>
       </div>
-      <div className="border border-2 mb-9">
-        <div className="font-bold">log</div>
-        <div className="grid gap-4 grid-cols-3 grid-rows-3">
-          {logs.map((log, idx) => (
-            <Card key={idx} {...log} />
+
+      {/* Section Logs */}
+      <div className="mb-9">
+        <div className="font-bold text-lg">log</div>
+        <div className="grid gap-2 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 grid-rows-3">
+          {logs.map((data, idx) => (
+            <Card key={idx} {...data} />
           ))}
         </div>
       </div>
-      <div className="border border-2 mb-9">
-        <div className="font-bold">newlsetter</div>
-        <div className="grid gap-4 grid-cols-3 grid-rows-3">
-          {logs.map((log, idx) => (
-            <Card key={idx} {...log} />
+
+      {/* Section Newsletters */}
+      <div className="mb-9">
+        <div className="font-bold text-lg">newlsetter</div>
+        <div className="grid gap-2 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 grid-rows-3">
+          {newsletters.map((data, idx) => (
+            <Card key={idx} {...data} />
           ))}
         </div>
       </div>
