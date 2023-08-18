9154cdc1a43b0249d7070d0d982bda4881bf457f
William Felker <gndclouds@icloud.com>
2023-08-18 14:44:37 -0700

    Updates and linting

--- app/nav.tsx
+++ app/nav.tsx
@@ -3,11 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-const navigation = [
-  { name: "thoughts", href: "/thoughts" },
-  { name: "things", href: "/things" },
-  { name: "about", href: "/about" },
-];
+const navigation = [{ name: "about", href: "/about" }];
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
