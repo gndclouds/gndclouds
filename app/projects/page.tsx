8c15556927c6f984da8f1ec55f3da39e1a1c722c
William Felker <gndclouds@icloud.com>
2023-10-15 09:05:41 -0700

    eod - init search

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,19 +1,48 @@
+"use client";
+
+import { useState } from "react";
 import { allProjects } from "@/.contentlayer/generated";
 import { compareDesc, format, parseISO } from "date-fns";
-
 import Link from "next/link";
 import Image from "next/image";
 
 export default function ProjectPage() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedYear, setSelectedYear] = useState("");
+
   const unpublishedProjects = allProjects.filter(
     (project) => project.published
   );
 
-  const projects = unpublishedProjects.sort((a, b) =>
+  let projects = unpublishedProjects.sort((a, b) =>
     compareDesc(new Date(a.publishedAt), new Date(b.publishedAt))
   );
+
+  // Filter projects by search query
+  if (searchQuery) {
+    projects = projects.filter((project) =>
+      project.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  }
+
+  // Filter projects by selected year
+  if (selectedYear) {
+    projects = projects.filter(
+      (project) =>
+        format(parseISO(project.publishedAt), "yyyy") === selectedYear
+    );
+  }
+
+  const uniqueYears = [
+    ...new Set(
+      allProjects.map((project) =>
+        format(parseISO(project.publishedAt), "yyyy")
+      )
+    ),
+  ];
+
   return (
-    <div className=" dark:prose-invert">
+    <div className="dark:prose-invert">
       <div className="p-4 min-w-screen flex ">
         {/* Hero Section */}
         <div className="relative flex-1 h-[200px] rounded-2xl overflow-hidden">
@@ -36,26 +65,51 @@ export default function ProjectPage() {
           <div className="absolute bottom-0 p-4 w-full">
             <div className="grid grid-cols-3 text-white uppercase font-bold text-smaller items-center">
               <div className="flex justify-start items-center">
-                Updated:{" "}
+                Newest:{" "}
                 <time dateTime={projects[0].publishedAt}>
-                  {format(parseISO(projects[0].publishedAt), "yyyy-MM-dd")}
+                  {format(parseISO(projects[0].publishedAt), "yyyy-MM")}
                 </time>
               </div>
               <div className="flex justify-center items-center">
-                Number of {allProjects.length}
+                {allProjects.length} Entries
               </div>
-              <div className="flex justify-end items-center">rss</div>
+              <div className="flex justify-end items-center">
+                Building{" "}
+                <Link href="/">
+                  Made For Earth <span className="font-mono">↗</span>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      {/* Projects Section */}
 
-      <div className="p-4 min-w-screen ">
+      {/* Projects Section */}
+      <div className="p-4">
+        <div className="mb-4">
+          <input
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            placeholder="Search projects..."
+            className="border border-gray-300 shadow-sm rounded-md px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent"
+          />
+          <select
+            value={selectedYear}
+            onChange={(e) => setSelectedYear(e.target.value)}
+          >
+            <option value="">Filter by year</option>
+            {uniqueYears.map((year) => (
+              <option key={year} value={year}>
+                {year}
+              </option>
+            ))}
+          </select>
+        </div>
         {projects.map((project) => (
           <article
             key={project._id}
-            className="grid grid-cols-1 sm:grid-cols-2 mx-2 m-20 sm:m-8 gap-4"
+            className="grid grid-cols-1 sm:grid-cols-2 my-20  gap-4 "
           >
             <div>
               <Link href={project.url || project.slug}>
@@ -73,17 +127,18 @@ export default function ProjectPage() {
               </div>
               <div className="mb-4 sm:mb-0">{project.description}</div>
             </div>
-            <div className=" relative aspect-video rounded-2xl overflow-hidden">
-              <Image
-                src={project.heroImage}
-                alt=""
-                quality={100}
-                fill
-                sizes="100vh"
-                style={{
-                  objectFit: "cover",
-                }}
-              />
+            <div className="relative aspect-video rounded-2xl overflow-hidden">
+              {project.heroImage ? (
+                <Image
+                  src={project.heroImage}
+                  alt=""
+                  quality={100}
+                  layout="fill"
+                  objectFit="cover"
+                />
+              ) : (
+                <div className="bg-[#f9d73b]"></div>
+              )}
             </div>
           </article>
         ))}
