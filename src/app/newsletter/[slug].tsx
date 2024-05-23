9ad5041046db72466cb6187af91128ce881e8ab1
William Felker <gndclouds@icloud.com>
2024-05-23 15:03:17 -0700

    eod

--- src/app/newsletter/[slug].tsx
+++ src/app/newsletter/[slug].tsx
@@ -5,26 +5,22 @@ import {
   Newsletter as ImportedNewsletter,
 } from "@/queries/newsletters";
 
-// Extend the ImportedNewsletter type if additional properties are needed locally
 type Newsletter = ImportedNewsletter & {
   additionalProperty?: string; // Example additional property
 };
 
 export default function NewsletterPage() {
   const router = useRouter();
-  const { slug } = router.query; // `slug` is an array of path segments
+  const { slug } = router.query;
   const [newsletter, setNewsletter] = useState<Newsletter | null>(null);
 
   useEffect(() => {
     async function fetchNewsletter() {
       if (slug) {
-        const newsletterSlug = Array.isArray(slug) ? slug.join("-") : slug;
+        const newsletterSlug = Array.isArray(slug)
+          ? slug.join("-").toLowerCase()
+          : slug.toLowerCase();
         const fetchedNewsletter = await getNewsletterBySlug(newsletterSlug);
-        if (fetchedNewsletter) {
-          console.log(fetchedNewsletter.title); // Outputs the title of the newsletter
-        } else {
-          console.log("Newsletter not found or error reading the file.");
-        }
         setNewsletter(fetchedNewsletter);
       }
     }
@@ -39,7 +35,7 @@ export default function NewsletterPage() {
   return (
     <div>
       <h1>{newsletter.title}</h1>
-      {/* <div dangerouslySetInnerHTML={{ __html: newsletter.metadata.contentHtml }} /> */}
+      <div dangerouslySetInnerHTML={{ __html: newsletter.content }} />
     </div>
   );
 }
