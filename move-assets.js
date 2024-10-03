ef44ce686f996e1c5607b0fb180912648a246c8e
William Felker <gndclouds@icloud.com>
2024-10-02 21:31:43 -0700

    fixing assets paths from submodule

---
+++ move-assets.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+
+const sourceDir = path.join(__dirname, "src/app/db/public/");
+const destDir = path.join(__dirname, "public/");
+
+function copyFiles(src, dest) {
+  if (!fs.existsSync(dest)) {
+    fs.mkdirSync(dest, { recursive: true });
+  }
+
+  fs.readdirSync(src).forEach((file) => {
+    const srcFile = path.join(src, file);
+    const destFile = path.join(dest, file);
+
+    if (fs.lstatSync(srcFile).isDirectory()) {
+      copyFiles(srcFile, destFile);
+    } else {
+      fs.copyFileSync(srcFile, destFile);
+    }
+  });
+}
+
+copyFiles(sourceDir, destDir);
+console.log("Assets moved to public directory.");
